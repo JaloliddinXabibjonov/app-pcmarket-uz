b78a34666db588f6a485f1cca268383598cdf5de
JaloliddinXabibjonov <jaloliddin0292@gmail.com>
2021-08-28 22:20:39 +0500

    edited security

--- src/main/java/uz/pcmarket/apppcmarketuz/config/Security.java
+++ src/main/java/uz/pcmarket/apppcmarketuz/config/Security.java
@@ -1,21 +1,47 @@
 package uz.pcmarket.apppcmarketuz.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class Security extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("SuperAdmin").password(passwordEncoder().encode("000")).roles("SUPER_ADMIN")
+                .and()
+                .withUser("Moderator").password(passwordEncoder().encode("111")).roles("MODERATOR")
+                .and()
+                .withUser("Operator").password(passwordEncoder().encode("222")).roles("OPERATOR");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
                 .csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/product/*").hasAnyRole("OPERATOR","SUPER_ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/product/*").hasAnyRole("MODERATOR","SUPER_ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/product/*").hasAnyRole("MODERATOR","SUPER_ADMIN")
+                .antMatchers("/api/product/**").hasRole("SUPER_ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/uz/pcmarket/apppcmarketuz/controller/ProductController.java
+++ src/main/java/uz/pcmarket/apppcmarketuz/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import uz.pcmarket.apppcmarketuz.entity.Product;
 import uz.pcmarket.apppcmarketuz.payload.ProductDto;
